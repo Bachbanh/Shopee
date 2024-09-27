49ac02824badb2cc86df3e1f8c03b44339234712
Bachbanh <167960120+Bachbanh@users.noreply.github.com>
2024-09-27 08:42:49 +0700

    Delete includes directory

--- includes/pdo.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=shopee2', 'root', ''); // Thay đổi tên người dùng và mật khẩu nếu cần
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Kết nối thất bại: " . $e->getMessage());
-}
-?>
